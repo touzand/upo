165c6676880b2bcfae03d5e03f0897297233a189
Alan Alonso <alan.alonso.gaf15@gmail.com>
2022-08-01 22:33:50 -0300

    Trending section added

--- src/pages/Home.page.js
+++ src/pages/Home.page.js
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Scroll from "../components/main-components/Scroll";
 import { api } from "../services/api";
 import Loader from "../components/Loader";
+import '../index.css'
 
 const HomeContainer = styled.div`
   color: whitesmoke;
@@ -11,6 +12,10 @@ const HomeContainer = styled.div`
   h2 {
     margin-left: 1rem;
   }
+
+  span{
+  color:var(--primal-color)
+  }
 `;
 
 const Home = () => (
@@ -18,13 +23,19 @@ const Home = () => (
     <Header endPoint={api.MOVIE_POPULAR_LIST} />
     {/*<Loader/>*/}
     <Scroll endPoint={api.MOVIE_POPULAR_LIST} mediaType="movie">
-      <h2>Top movies</h2>
+      <h2><span>- </span>Top movies</h2>
     </Scroll>
     <Scroll endPoint={api.TV_POPULAR_LIST} mediaType="tv">
-      <h2>Top series and TV shows</h2>
+      <h2><span>- </span>Top series and TV shows</h2>
     </Scroll>
     <Scroll endPoint={api.MOVIE_UPCOMING} mediaType="movie">
-      <h2>Upcoming movies</h2>
+      <h2><span>- </span>Upcoming movies</h2>
+    </Scroll>
+    <Scroll endPoint={api.TV_TRENDINGS} mediaType="tv">
+      <h2><span>- </span>Trending tv and series</h2>
+    </Scroll>
+    <Scroll endPoint={api.MOVIE_TRENDINGS} mediaType="movie">
+      <h2><span>- </span>Trending movies</h2>
     </Scroll>
   </HomeContainer>
 );
--- src/services/api.js
+++ src/services/api.js
@@ -12,4 +12,6 @@ export const api = {
   TV_TOP_RATED:`${API_ENDPOINT}/tv/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
   MOVIE_UPCOMING:`${API_ENDPOINT}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`,
   TV_LATES:`https://api.themoviedb.org/3/tv/changes?api_key=8e78295ff9a0d66c6596e756caa8add1&language=en-US`,
+  TV_TRENDINGS:`${API_ENDPOINT}/trending/tv/day?api_key=${API_KEY}`,
+  MOVIE_TRENDINGS:`${API_ENDPOINT}/trending/movie/day?api_key=${API_KEY}`,
 }
