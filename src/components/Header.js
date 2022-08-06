2ce9d3536be9f323bc8ca551473c9f111d4ecc17
Alan Alonso <alan.alonso.gaf15@gmail.com>
2022-08-05 21:59:41 -0300

    header desktop version updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,13 @@ import { useState } from "react";
 import styled from "styled-components";
 import SearchBar from "../components/SearchBar";
 import useAxios from "../hooks/useAxios";
-import {api} from "../services/api"
-import '../index.css'
+import { api } from "../services/api";
+import "../index.css";
 
 const HeaderContainer = styled.header`
   text-align: left;
-  background-image:linear-gradient(to top,var(--dark),#2228),url( ${props => props.api}${props=>props.backgroundPath} );
+  background-image:linear-gradient(to top,var(--dark),#2228),url( ${(props) =>
+    props.api}${(props) => props.backgroundPath} );
   color:white;
   font-weight:bold;
   background-size: cover;
@@ -45,7 +46,8 @@ const HeaderContainer = styled.header`
   }
 
   @media (min-width:800px){
-  background-image:linear-gradient(to right,var(--dark),#2228),url( ${props => props.api}${props=>props.backgroundPath} );
+  background-image:linear-gradient(to right,var(--dark),#2228),url( ${(props) =>
+    props.api}${(props) => props.backgroundPath} );
 
   h1{
   width:400px;
@@ -56,15 +58,46 @@ const HeaderContainer = styled.header`
   }
 
   .bg-information{
-  display:block;
+  display:flex;
+align-items:center;
   text-align:right;
+  color:#ddd3;
+  gap:2rem;
+
+  &:hover{
+  img{
+  opacity:1;
+
+  }
+  }
+
+  img{
+  width:100px;
+  border-radius:.25rem;
+  opacity:.4;
+  }
+
+
+  *{
+  margin:0;
+
+  }
+
+  div *:hover{
+  color:white;
+  }
   }
   
   .desktop-bg-information{
 display:flex;
 justify-content:space-between;
+align-items:center;
   padding:0 4rem;
+
   }
+
+
+
 
   }
 
@@ -78,28 +111,43 @@ const Header = (props) => {
     method: "get",
   });
 
+  let randomMediaIndex = Math.floor(Math.random() * 19);
+
   return (
     <div>
-      {!isLoading && 
-          <HeaderContainer backgroundPath={response.data.results[ Math.floor(Math.random() * response.data.results.length) ].backdrop_path} api={api.POSTER}>
-            <div className="desktop-bg-information">
+      {!isLoading && (
+        <HeaderContainer
+          backgroundPath={response.data.results[randomMediaIndex].backdrop_path}
+          api={api.POSTER}
+        >
+          {console.log(response.data.results[randomMediaIndex])}
+          <div className="desktop-bg-information">
             <div>
-      <h1>A massive information base about a lot of movies and tv shows</h1>
-      <button onClick={() => props.setInputVisible(true)}>Quick search</button>
-    </div>
-    <div className="bg-information">
-      <h4>Hola mundo locoo</h4>
-
-    </div>
+              <h1>
+                A massive information base about a lot of movies and tv shows
+              </h1>
+              <button onClick={() => props.setInputVisible(true)}>
+                Quick search
+              </button>
             </div>
-      {inputVisible && (
-        <SearchBar
-          inputVisible={props.inputVisible}
-          setInputVisible={props.setInputVisible}
-        />
+            <div className="bg-information">
+              <img src={`${api.POSTER}${ response.data.results[randomMediaIndex].poster_path }`}/>
+        <div>
+          <h4>{response.data.results[randomMediaIndex].title}</h4>
+          <h4>{response.data.results[randomMediaIndex].release_date}</h4>
+          <h4>{response.data.results[randomMediaIndex].vote_average}</h4>
+          <h4>{response.data.results[randomMediaIndex].popularity}</h4>
+
+        </div>            </div>
+          </div>
+          {inputVisible && (
+            <SearchBar
+              inputVisible={props.inputVisible}
+              setInputVisible={props.setInputVisible}
+            />
+          )}
+        </HeaderContainer>
       )}
-    </HeaderContainer>
-      }
     </div>
   );
 };
--- src/components/main-components/ScrollCard.js
+++ src/components/main-components/ScrollCard.js
@@ -1,16 +1,17 @@
-import styled,{keyframes} from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { Link } from "react-router-dom";
+import Percent from '../Percent'
 
 const fadeIn = keyframes`
 0%{top:8rem}
 100%{top:0}
-`
+`;
 
 const Card = styled.div`
   height: auto;
   margin-left: 0.5rem;
-  position:relative;
-  animation: ${fadeIn} 1s ${props => props.animationDelay}s ease both;
+  position: relative;
+  animation: ${fadeIn} 1s ${(props) => props.animationDelay}s ease both;
 
   img {
     border-radius: 0.25rem;
@@ -21,18 +22,48 @@ const Card = styled.div`
     margin-top: 0.5rem;
   }
 
-  @media (min-width:800px){
+  div {
+    display: none;
+  }
 
-img{
-width:150px;
-}
+  @media (min-width: 800px) {
+  margin-left:1rem;
+
+    img {
+      width: 150px;
+      height: 225px;
+      border-radius:.25rem .25rem 0rem 0rem;
+    }
+
+    div {
+      display: flex;
+      flex-direction: column;
+      color:white;
+      margin-bottom:1rem;
+      align-items:right;
+
+      h4{
+      margin:.5rem 0;
+      }
+
+  span{
+  color:var(--dark) !important;
+  font-weight:bold;
+background-color:var(--dark-80) !important;
+
+      }
+    }
   }
 `;
 
-const SlideCard = ({ props,mediaType,animationDelay }) => (
+const SlideCard = ({ props, mediaType, animationDelay }) => (
   <Link to={`upo/${mediaType}/${props.id}`}>
     <Card animationDelay={animationDelay}>
       <img src={`https://image.tmdb.org/t/p/w500/${props.poster_path}`} />
+      <div>
+        <Percent>{props.vote_average}</Percent>
+        <h4>{props.title || props.name}</h4>
+      </div>
     </Card>
   </Link>
 );
